140bca2424fcf3084c3508484c799594cadf0bba
isabelle Lima <a39617@alunos.ipb.pt>
2023-08-14 16:13:44 +0100

    melhorei o codigo

--- GESSWIN/App.js
+++ GESSWIN/App.js
@@ -1,26 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TouchableOpacity,Image} from 'react-native';
 import  GuessTheWinner  from './componetes/GuessComponent'
+import { useState } from 'react';
 
 
 
 export default function App() {
   const teams = [ 'Benfica','Inter','Man City','Bayern', 'Real Madrid', 'Chelsea'];
+  const [showGesscompt,SetshowGesscompt] = useState(false);
+  const GuessComponent = () => {
+    SetshowGesscompt(!showGesscompt);
+    
+  };
+
+  const backmenu = () =>{
+
+    SetshowGesscompt(!showGesscompt);
+  }
+
   return (
    <View>
-    <GuessTheWinner Teams={teams}/>
+    {showGesscompt ? (
+       <View>
+      <GuessTheWinner Teams={teams} />
+      <Image style={{width:300,height:300,marginBottom:10,alignSelf:'center',marginTop:30}} source={require('./assets/winn.gif')}/> 
+       <TouchableOpacity
+       onPress={GuessComponent}
+       style={{  width:350,
+         fontSize: 15,
+         padding: 10,
+         backgroundColor: '#3C464E',
+         borderRadius: 50,
+         color:'#fff',
+         top:70,
+         alignSelf:'center'}}>
+       <Text style ={{color:'#059669', alignSelf:'center', borderRadius:20}}>Gess the winner again ! </Text>
+     </TouchableOpacity>
+     </View>
+    ): (
+      <View>
+       <Image style={{width:300,height:300,marginBottom:10,alignSelf:'center',marginTop:30}} source={require('./assets/GESS.gif')}/> 
+        <TouchableOpacity
+        onPress={GuessComponent}
+        style={{  width:350,
+          fontSize: 15,
+          padding: 10,
+          backgroundColor: '#3C464E',
+          borderRadius: 50,
+          color:'#fff',
+          top:100,
+          alignSelf:'center'}}>
+        <Text style ={{color:'#059669', alignSelf:'center', borderRadius:20}}>Gess the winner! </Text>
+      </TouchableOpacity>
+    </View>
+    )}
+  
    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 
 
 
--- GESSWIN/componetes/GuessComponent.js
+++ GESSWIN/componetes/GuessComponent.js
@@ -1,16 +1,17 @@
 import * as React from 'react';
-import { Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
-
-function GuessTheWinner({Teams}) {
-  
+function GuessTheWinner({ Teams }) {
   const Index = Math.floor(Math.random() * Teams.length);
   let winner = Teams[Index];
 
-  return(
-     <View> 
-        <Text style= {{padding: 40, fontSize:30, color:'black'}} >The winner will be {winner} </Text>
+  return (
+    <View>
+      <Text style={{ padding: 40, fontSize: 30, color: 'black' }}>
+        The winner will be {winner}
+      </Text>
     </View>
-    );
+  );
 }
+
 export default GuessTheWinner;
